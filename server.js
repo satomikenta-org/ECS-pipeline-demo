988aa6c9938a1b99d24422584f115f4581b01a37
里見健太 <satomikennta1213@gmail.com>
2019-05-18 20:14:55 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/healthcheck', (req, res) => {
+  res.send('OK');
+});
+
+app.get('/', (req, res) => {
+  console.log("GET /");
+  res.send("Version 1");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
